7545b8dbf6c6dfe0a35ec6b512071c4dcfa62c55
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-11-10 20:46:28 -0500

    Se agrega const para contolar los datos obligatorios que deben venir desde el front en la creación de productos

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-import Express from "express";
+import Express from 'express';
 
 const app = Express()
+app.use(Express.json())
 
 app.get('/productos', (req, res) => {
-    console.log("Se hizo una llamadaaaa");
+    console.log("Se hizo una llamada a /productos");
     const productos = [
-    {id:"001", nombre:"cama", valor:"20.000", estado:"activo",};
-]
+    {id:"001", nombre:"cama", valor:"20.000", estado:"activo",},
+    {id:"002", nombre:"mesa", valor:"100.000", estado:"activo",}
+
+];
 res.send(productos);
 });
 
-app.post('/producto/nuevo', (req, res) => {
-    res.send('Ok, Vehículo creado');
-});
+app.post('/productos/nuevo', (req, res) => {
+    // Acá va el código para crear el producto
+    const datosProductos = req.body;
+    console.log('llaves: ', Object.keys(datosProductos));
+    try {
+        
+        if (
+            Object.keys(datosProductos).includes('id') &&
+            Object.keys(datosProductos).includes('nombre') &&
+            Object.keys(datosProductos).includes('valor')  &&
+            Object.keys(datosProductos).includes('estado')
+        ) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch {
+        res.sendStatus(500);
+     }
+    });
 
 app.listen(4000, () => {
     console.log('Escuchando en el puerto 4000')
