c6c1bbcdc81172f941c0ac8de65e10ff3ec933d4
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-11-10 19:49:35 -0500

    Se crea la primer solicitud POST

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ import Express from "express";
 const app = Express()
 
 app.get('/productos', (req, res) => {
-    console.log("Se gizo una llamadaaaa")
-    res.send("<h1>HOLA PUESSS</h1>")
+    console.log("Se hizo una llamadaaaa");
+    const productos = [
+    {id:"001", nombre:"cama", valor:"20.000", estado:"activo",};
+]
+res.send(productos);
+});
+
+app.post('/producto/nuevo', (req, res) => {
+    res.send('Ok, Vehículo creado');
 });
 
 app.listen(4000, () => {
