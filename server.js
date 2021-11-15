9d3b0560f807c30a8067097d0c876c7204d7f3bf
Oscar Clavijo <oscar.clavijo90@gmail.com>
2021-11-15 09:44:57 -0500

    Se crean las rutas de usuarios

--- server.js
+++ server.js
@@ -59,6 +59,50 @@ app.post('/productos/nuevo', (req, res) => {
      }
  });
 
+ app.get('/usuarios', (req, res) => {
+    console.log("Se hizo una llamada a /usuarios");
+    BaseMongo
+    .collection('usuario')
+    .find({})
+    .limit(100)
+    .toArray((err,result) => {
+        if (err){
+         res.status(500).send('Error al consultar los usuarios')
+        }else {
+          res.json(result);
+    }
+});
+
+});
+ app.post('/usuarios/nuevo', (req, res) => {
+    const datosUsuarios = req.body;
+    console.log('llaves: ', Object.keys(datosUsuarios));
+    try {
+        if (
+            Object.keys(datosUsuarios).includes('cedula') &&
+            Object.keys(datosUsuarios).includes('nombre') &&
+            Object.keys(datosUsuarios).includes('rol')  &&
+            Object.keys(datosUsuarios).includes('estado')
+        ) {
+           BaseMongo.collection('usuario').insertOne(datosUsuarios, (err, result) => {
+                if (err){
+                    console.error(err);
+                    res.sendStatus(500);
+                }else {
+                    console.log(result)
+                     res.sendStatus(200);
+                }
+            });
+
+        } else {
+            res.sendStatus(500);
+        }
+    } catch {
+        res.sendStatus(500);
+     }
+ });
+
+
  app.patch('/productos/editar', (req, res) => {
      const edicion = req.body;
      console.log(edicion);
@@ -78,20 +122,54 @@ app.post('/productos/nuevo', (req, res) => {
      });
  });
 
+ app.patch('/usuarios/editar', (req, res) => {
+    const edicion = req.body;
+    console.log(edicion);
+    const filtro = {_id: new ObjectId (edicion.id)};
+    delete edicion.id
+    const operacion = {
+        $set:edicion
+    }
+    BaseMongo.collection('usuario').findOneAndUpdate(filtro, operacion, { upsert: true, returOriginal: true}, (err, result) => {
+       if (err){
+           console.error('Error actualizando el usuario', err);
+           res.sendStatus(500);
+       }else {
+           console.log('Actualizado con éxito')
+            res.sendStatus(200);
+       }
+    });
+});
+
  app.delete('/productos/eliminar', (req, res) => {
 
-     const filtro = {_id: new ObjectId (req.body.id)};
-     BaseMongo.collection('producto').deleteOne(filtro,(err, result)=>{
+    const filtro = {_id: new ObjectId (req.body.id)};
+    BaseMongo.collection('producto').deleteOne(filtro,(err, result)=>{
 
-        if (err){
-            console.error('Error eliminando el producto', err);
-            res.sendStatus(500);
-        }else {
-            console.log('Actualizado con éxito')
-             res.sendStatus(200);
-        }
-     })
- });
+       if (err){
+           console.error('Error eliminando el producto', err);
+           res.sendStatus(500);
+       }else {
+           console.log('Actualizado con éxito')
+            res.sendStatus(200);
+       }
+    })
+});
+
+app.delete('/usuarios/eliminar', (req, res) => {
+
+    const filtro = {_id: new ObjectId (req.body.id)};
+    BaseMongo.collection('usuario').deleteOne(filtro,(err, result)=>{
+
+       if (err){
+           console.error('Error eliminando el usuario', err);
+           res.sendStatus(500);
+       }else {
+           console.log('Actualizado con éxito')
+            res.sendStatus(200);
+       }
+    })
+});
 
 const main = () => {
     client.connect((err, db)=>{
